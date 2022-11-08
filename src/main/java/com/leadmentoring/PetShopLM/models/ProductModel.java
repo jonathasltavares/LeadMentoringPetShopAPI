5b3cd74d4fb69d6b6eb437b78f6b01d2605fffdf
Jonathas Tavares <jonathasl.tavares@gmail.com>
2022-11-08 15:19:27 -0300

    ProductModel

--- src/main/java/com/leadmentoring/PetShopLM/models/ProductModel.java
+++ src/main/java/com/leadmentoring/PetShopLM/models/ProductModel.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 @Entity
 @Data
 @Table(name="product")
-public class Product implements Serializable {
+public class ProductModel implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
