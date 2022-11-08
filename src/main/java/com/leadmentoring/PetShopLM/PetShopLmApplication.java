4344c83a3eb025289cbab07be40212d0366e550e
Jonathas Tavares <jonathasl.tavares@gmail.com>
2022-11-08 12:19:35 -0300

    first commit

---
+++ src/main/java/com/leadmentoring/PetShopLM/PetShopLmApplication.java
@@ -0,0 +1,13 @@
+package com.leadmentoring.PetShopLM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopLmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShopLmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leadmentoring/PetShopLM/PetShopLmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leadmentoring.PetShopLM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopLmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
